0540061d62520c27d6a837c8409e8bde98c180fc
Johan Hedberg <johan.hedberg@intel.com>
2021-02-20 14:26:51 +0200

    dts: Support app independent but board specific DTS overlays
    For any driver development that happens in the internal tree for
    boards that are already upstream we need a way to extend the DTS using
    an overlay. We do this by taking advantage of the DTC_OVERLAY_FILE
    mechanism, while also preserving support for app-specific overlay
    files in the app root directory.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- share/zephyrbuild-package/cmake/ZephyrBuildConfig.cmake
+++ share/zephyrbuild-package/cmake/ZephyrBuildConfig.cmake
@@ -10,3 +10,20 @@ get_filename_component(APPLICATION_PROJECT_DIR
 list(APPEND BOARD_ROOT ${APPLICATION_PROJECT_DIR})
 list(APPEND SOC_ROOT ${APPLICATION_PROJECT_DIR})
 list(APPEND DTS_ROOT ${APPLICATION_PROJECT_DIR})
+
+# To support driver development in the internal tree for boards that are
+# already upstream, allow having an app-independent overlay file for
+# specific boards under overlay/<board>.overlay. Do this without losing
+# support for app-specific overlays, although in a limited manner (for
+# the most common <board>.overlay file convention in the root directory
+# of the application.
+set(INTEL_OVERLAY ${APPLICATION_PROJECT_DIR}/overlay/${BOARD}.overlay)
+set(BOARD_OVERLAY ${APPLICATION_SOURCE_DIR}/${BOARD}.overlay)
+
+if(EXISTS ${INTEL_OVERLAY})
+  if (EXISTS ${BOARD_OVERLAY})
+    set(DTC_OVERLAY_FILE "${INTEL_OVERLAY} ${BOARD_OVERLAY}")
+  else()
+    set(DTC_OVERLAY_FILE ${INTEL_OVERLAY})
+  endif()
+endif()
