925c6359f775ec01180fb14662be713a80154046
Jiang Wei W <wei.w.jiang@intel.com>
2021-07-16 22:04:07 +0800

    drivers: ipc: fit ipc syscall handler to zephyr2p6
    fit ipc syscall handler to zephyr2p6
    ut done
    
    Signed-off-by: Jiang Wei W <wei.w.jiang@intel.com>

--- drivers/ipc/ipc_handlers.c
+++ drivers/ipc/ipc_handlers.c
@@ -4,19 +4,25 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <ipc.h>
+#include <drivers/ipc.h>
 #include <string.h>
 #include <syscall_handler.h>
 
 #define MAX_IPC_MSG_LEN 128
 
-Z_SYSCALL_HANDLER(ipc_read_drbl, dev, drbl) {
+static inline int z_vrfy_ipc_read_drbl(const struct device *dev, uint32_t *drbl)
+{
 	Z_OOPS(Z_SYSCALL_DRIVER_IPC(dev, ipc_read_drbl));
 	Z_OOPS(Z_SYSCALL_MEMORY_WRITE(drbl, sizeof(uint32_t)));
-	return z_impl_ipc_read_drbl((struct device *)dev, (uint32_t *)drbl);
+	return z_impl_ipc_read_drbl(dev, drbl);
 }
+#include <syscalls/ipc_read_drbl_mrsh.c>
 
-Z_SYSCALL_HANDLER(ipc_read_msg, dev, drbl, msg, msg_size) {
+static inline int z_vrfy_ipc_read_msg(const struct device *dev,
+				      uint32_t *drbl,
+				      uint8_t *msg,
+				      uint32_t msg_size)
+{
 	Z_OOPS(Z_SYSCALL_DRIVER_IPC(dev, ipc_read_msg));
 	if (drbl) {
 		Z_OOPS(Z_SYSCALL_MEMORY_WRITE(drbl, sizeof(uint32_t)));
@@ -24,27 +30,32 @@ Z_SYSCALL_HANDLER(ipc_read_msg, dev, drbl, msg, msg_size) {
 	if (msg) {
 		Z_OOPS(Z_SYSCALL_MEMORY_WRITE(msg, msg_size));
 	}
-	return z_impl_ipc_read_msg((struct device *)dev,
-				   (uint32_t *)drbl, (uint8_t *)msg, msg_size);
+	return z_impl_ipc_read_msg(dev, drbl, msg, msg_size);
 }
+#include <syscalls/ipc_read_msg_mrsh.c>
 
-Z_SYSCALL_HANDLER(ipc_send_ack, dev, ack_drbl, ack_msg, ack_msg_size) {
+
+static inline int z_vrfy_ipc_send_ack(const struct device *dev,
+				      uint32_t ack_drbl,
+				      uint8_t *ack_msg,
+				      uint32_t ack_msg_size)
+{
 	Z_OOPS(Z_SYSCALL_DRIVER_IPC(dev, ipc_send_ack));
 	if (ack_msg) {
 		Z_OOPS(Z_SYSCALL_MEMORY_READ(ack_msg, ack_msg_size));
 	}
-	return z_impl_ipc_send_ack((struct device *)dev,
-				   ack_drbl, (uint8_t *)ack_msg, ack_msg_size);
+	return z_impl_ipc_send_ack(dev, ack_drbl, ack_msg, ack_msg_size);
 }
+#include <syscalls/ipc_send_ack_mrsh.c>
 
-Z_SYSCALL_HANDLER(ipc_write_msg, dev, drbl, msg, msg_size, ack_drbl,
-		  more_args_ptr) {
-	volatile struct _syscall_7_args *margs =
-		(struct _syscall_7_args *)more_args_ptr;
-
-	uint8_t *ack_msg = (uint8_t *)margs->arg6;
-	uint32_t ack_msg_size = (uint32_t)margs->arg7;
-
+static inline int z_vrfy_ipc_write_msg(const struct device *dev,
+				       uint32_t drbl,
+				       uint8_t *msg,
+				       uint32_t msg_size,
+				       uint32_t *ack_drbl,
+				       uint8_t *ack_msg,
+				       uint32_t ack_msg_size)
+{
 	if (msg_size > MAX_IPC_MSG_LEN) {
 		return -EINVAL;
 	}
@@ -62,8 +73,8 @@ Z_SYSCALL_HANDLER(ipc_write_msg, dev, drbl, msg, msg_size, ack_drbl,
 		Z_OOPS(Z_SYSCALL_MEMORY_WRITE(ack_msg, ack_msg_size));
 	}
 	Z_OOPS(Z_SYSCALL_DRIVER_IPC(dev, ipc_write_msg));
-	return z_impl_ipc_write_msg((struct device *)dev,
-				    drbl, (uint8_t *)copy, msg_size,
-				    (uint32_t *)ack_drbl, ack_msg, ack_msg_size);
+	return z_impl_ipc_write_msg(dev, drbl, (uint8_t *)copy, msg_size,
+				    ack_drbl, ack_msg, ack_msg_size);
 }
 
+#include <syscalls/ipc_write_msg_mrsh.c>
